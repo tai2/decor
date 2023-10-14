f3899fbbddba8bea88207b22ab8c04719850a283
tai2 <hello@tai2.net>
2023-10-14 19:31:19 +0900

    Implement the main pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { marked } from "./deps/marked.ts";
+import { parseTemplate } from "./parse_template.ts";
+import { Parser } from "./parser.ts";
+import { templateRenderer } from "./template_renderer.ts";
+import assets from "./assets.json" assert { type: "json" };
 
-const tokens = marked.lexer("Hello, *World!*");
-console.log(tokens);
+function main() {
+  const template = parseTemplate(assets.defaultTemplate);
+
+  const renderer = templateRenderer(template);
+
+  const tokens = marked.lexer(assets.defaultContent);
+
+  const output = Parser.parse(renderer, tokens);
+
+  console.log(output);
+}
+
+main();
