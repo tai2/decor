efa285769b514cc249e9f0b8636fdcacea8c81f4
GitHub <noreply@github.com>
2024-03-01 19:46:19 +0000

    Update dependencies

--- src/deps/std/flags.ts
+++ src/deps/std/flags.ts
@@ -1,1 +1,1 @@
-export * from 'https://deno.land/std@0.217.0/flags/mod.ts'
+export * from 'https://deno.land/std@0.218.2/flags/mod.ts'
--- src/deps/std/path.ts
+++ src/deps/std/path.ts
@@ -1,1 +1,1 @@
-export * from 'https://deno.land/std@0.217.0/path/mod.ts'
+export * from 'https://deno.land/std@0.218.2/path/mod.ts'
