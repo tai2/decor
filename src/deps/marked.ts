d345d102c9185e0d7bd7387dae3247fc59418407
GitHub <noreply@github.com>
2025-04-25 19:46:05 +0000

    Update dependencies

--- src/deps/marked.ts
+++ src/deps/marked.ts
@@ -1,11 +1,11 @@
-export { marked } from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.9/src/marked.ts'
+export { marked } from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.11/src/marked.ts'
 export {
   cleanUrl,
   escape,
   unescape,
-} from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.9/src/helpers.ts'
+} from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.11/src/helpers.ts'
 export type {
   Token,
   Tokens,
-} from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.9/src/Tokens.ts'
-export { _TextRenderer } from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.9/src/TextRenderer.ts'
+} from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.11/src/Tokens.ts'
+export { _TextRenderer } from 'https://raw.githubusercontent.com/markedjs/marked/v15.0.11/src/TextRenderer.ts'
